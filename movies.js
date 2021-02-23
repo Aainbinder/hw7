0395f0c6e50ddf1db8c440cf4e93ca441a197941
Aainbinder <Andrew.ainbinder@gmail.com>
2021-02-22 22:11:34 -0600

    final

--- movies.js
+++ movies.js
@@ -6,18 +6,25 @@ firebase.auth().onAuthStateChanged(async function(user) {
     let json = await response.json()
     let movies = json.results
 
+    db.collection('users').doc(user.uid).set({
+      name: user.displayName,
+      email: user.email
+    })
+
+    let query = await db.collection('watched').where('userId','==',`${user.uid}`).get() //Not efficient to call a database within a loop because page noticeably loads one movie at a time. Moved query outside and added opacity after populating page
+    let watchList = query.docs
 
     for (let i=0; i<movies.length; i++) {
       let movie = movies[i]
-      let docRef = await db.collection('watched').doc(`${movie.id}`).get()
-      let watchedMovie = docRef.data()
-      let opacityClass = ''
-      if (watchedMovie) {
-        opacityClass = 'opacity-20'
-      }
+      //let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
+      //let watchedMovie = docRef.data()
+      //let opacityClass = ''
+      //  if (watchedMovie) {
+      //    opacityClass = 'opacity-20'
+      //  }
 
       document.querySelector('.movies').insertAdjacentHTML('beforeend', `
-        <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
+        <div class="w-1/5 p-4 movie-${movie.id}">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
           <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
         </div>
@@ -27,9 +34,33 @@ firebase.auth().onAuthStateChanged(async function(user) {
         event.preventDefault()
         let movieElement = document.querySelector(`.movie-${movie.id}`)
         movieElement.classList.add('opacity-20')
-        await db.collection('watched').doc(`${movie.id}`).set({})
+        await db.collection('watched').doc(`${movie.id}-${user.uid}`).set({
+          movieId: movie.id,
+          userId: user.uid
+        })
       }) 
+
+      document.querySelector('.sign-in-or-sign-out').innerHTML = ` ${user.displayName}
+      <button class="text-pink-500 underline sign-out">Sign Out</button>
+      `
+
+      document.querySelector('.sign-out').addEventListener('click', function(event) {
+        console.log('sign out clicked')
+        firebase.auth().signOut()
+        document.location.href = 'movies.html'
+      })
     }
+
+    for(let i = 0; i < watchList.length; i++){ //Add opacity class
+      let movieId = watchList[i].data().movieId
+
+      let target = document.querySelector(`.movie-${movieId}`)
+      if(target){
+        target.classList.add('opacity-20')
+      }
+    }
+
+
   } else {
         // Initializes FirebaseUI Auth
         let ui = new firebaseui.auth.AuthUI(firebase.auth())
